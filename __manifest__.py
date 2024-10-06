1fa5f37db57612c8b34d3b1e4af669e0c5d0646a
leonelkenfack <realleonelkenfack@gmail.com>
2024-10-06 06:43:22 +0100

    Comment model and pos_config_setting views

--- __manifest__.py
+++ __manifest__.py
@@ -7,9 +7,9 @@
     'category': 'Point of Sale',
     'version': '0.1',
     'depends': ['point_of_sale'],
-    'data': [
-        'views/pos_config_view.xml',
-    ],
+    # 'data': [
+    #     'views/pos_config_view.xml',
+    # ],
     'assets': {
         'point_of_sale.assets_prod': [
             'fiscal_cash_register/static/src/js/payment_screen.js',
